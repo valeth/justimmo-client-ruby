fbbd506600d1da91708a2225b7d5b746663bc115
Patrick Auernig <patrick.auernig@mykolab.com>
2017-05-13 07:13:03 +0200

    Add example

---
+++ examples/client.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby
+
+require 'justimmo'
+require 'dotenv'
+require 'pp'
+
+Dotenv.load
+
+Justimmo.configure do |config|
+  config.username = ENV['JUSTIMMO_USERNAME']
+  config.password = ENV['JUSTIMMO_PASSWORD']
+end
+
+pp Justimmo::Realty.find({ zip_code: 6800 }, limit: 5)
