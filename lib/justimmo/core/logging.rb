784775daf043971d62344ec445fdcc2eecfe7f4f
Patrick Auernig <patrick.auernig@mykolab.com>
2017-07-31 23:18:26 +0200

    Move logging code into Logging module

--- lib/justimmo/core/logging.rb
+++ lib/justimmo/core/logging.rb
@@ -1,12 +1,37 @@
-require 'justimmo/api'
+# frozen_string_literal: true
+
+require 'logger'
 
 module Justimmo
-  module Core
-    # Logging support
-    module Logging
+  # Logging support
+  module Logging
+    class << self
+      # Use the Rails or default logger if none is set.
+      # @!attribute [rw] logger
+      # @return [Logger]
       def logger
-        Justimmo.logger
+        @logger ||= rails_logger || default_logger
       end
+
+      attr_writer :logger
+
+      def default_logger
+        logger = Logger.new($stdout)
+        logger.level = Logger::INFO
+        logger
+      end
+
+      # The Ruby on Rails logger
+      # @return [Logger, nil] The logger object
+      def rails_logger
+        if ('true' == ENV.fetch('JUSTIMMO_USE_RAILS_LOGGER', 'true')) && defined?(::Rails)
+          ::Rails&.logger
+        end
+      end
+    end
+
+    def logger
+      Logging.logger
     end
   end
 end
