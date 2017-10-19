a3bdcbf0d6ac83d04565f4567f61bac24734ba2d
Patrick Auernig <patrick.auernig@mykolab.com>
2017-10-19 13:01:41 +0200

    Add to_a, to_s and map methods for RealtyMarketing model

--- lib/justimmo_client/api/v1/models/realty_marketing.rb
+++ lib/justimmo_client/api/v1/models/realty_marketing.rb
@@ -8,5 +8,22 @@ module JustimmoClient::V1
     #   @return [$2]
     attribute :buy,  Boolean
     attribute :rent, Boolean
+
+    # @!group Instance Method Summary
+
+    def to_a
+      tmp = []
+      tmp << :buy if buy?
+      tmp << :rent if rent?
+      tmp
+    end
+
+    def to_s
+      to_a.join(", ")
+    end
+
+    def map(&block)
+      to_a.map(&block)
+    end
   end
 end
