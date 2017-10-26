2758952bcbba3d56b77ccb899c9837c723edf0ac
Patrick Auernig <patrick.auernig@mykolab.com>
2017-10-26 16:12:27 +0200

    Update spec_helper.rb
    Add WebMock and FactoryGirl and update configuration.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,35 @@
 require "bundler/setup"
 require "simplecov"
+require "webmock/rspec"
+require "factory_girl"
+require "retriable"
 
 require "justimmo_client"
 
-# Disable the logger for tests
-JustimmoClient::Logging.logger = Logger.new(nil)
-
 RSpec.configure do |config|
+  config.include FactoryGirl::Syntax::Methods
+
+  config.before(:suite) do
+    FactoryGirl.find_definitions
+
+    # Mock configuration
+    JustimmoClient.configure do |c|
+      c.username = "user"
+      c.password = "pass"
+      c.base_url = "testing"
+    end
+
+    # Disable the logger
+    JustimmoClient::Logging.logger = Logger.new(nil)
+
+    Retriable.configure do |c|
+      c.tries = 1
+      c.base_interval = 1.0
+      c.multiplier = 1.0
+      c.rand_factor = 0.0
+    end
+  end
+
   # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = ".rspec_status"
 
@@ -15,6 +38,6 @@ RSpec.configure do |config|
   end
 end
 
-def fixture(name, path)
-  File.read(File.join(__dir__, "data", path, "#{name}.xml"))
+def fixture(path)
+  File.read(File.join(__dir__, "fixtures", path))
 end
